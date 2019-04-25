f88c063801265df8cb2928f1b4f4875ddb398424
Jerome Bakker <jeabakker@coldtrick.com>
2019-04-25 15:18:39 +0200

    fixed: only show size warnings for icons

--- views/default/icon_cropper/messages.php
+++ views/default/icon_cropper/messages.php
@@ -1,6 +1,13 @@
 <?php
+/**
+ * Show warning about size constraints when uploading/cropping an icon
+ *
+ * @uses $vars['show_icon_cropper_messages'] Show messages (default: true for icon_type = 'icon', false otherwise)
+ */
 
-if (!(bool) elgg_extract('show_icon_cropper_messages', $vars, true)) {
+$icon_type = elgg_extract('icon_type', $vars, 'icon');
+$show_icon_cropper_messages = (bool) elgg_extract('show_icon_cropper_messages', $vars, $icon_type === 'icon');
+if (!$show_icon_cropper_messages) {
 	return;
 }
 
